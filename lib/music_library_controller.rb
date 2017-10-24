d931a79af1c526584808bf0a1b0afb8552a8aec7
Adam Fudge <coolfudge@gmail.com>
2017-10-24 00:16:52 +0000

    automatically backed up by learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -49,17 +49,17 @@ class MusicLibraryController
   end
 
   def list_artists
-    sorted_songs = Song.all.sort_by {|artist| artist.name}
-    sorted_songs.each_with_index do |song, line_number|
-      puts "#{line_number+1}. #{song.artist.name} - #{song.name} - #{song.genre.name}"
+    sorted_artists = Artist.all.sort_by {|artist| artist.name}
+    sorted_artists.each_with_index do |artist, line_number|
+      puts "#{line_number+1}. #{artist.name}"
     end
   end
 
   def list_genres
-    sorted_songs = Song.all.sort_by {|genre| genre.name}
-    sorted_songs.each_with_index do |song, line_number|
-      puts "#{line_number+1}. #{song.artist.name} - #{song.name} - #{song.genre.name}"
-    end
+    # sorted_songs = Song.all.sort_by {|genre| genre.name}
+    # sorted_songs.each_with_index do |song, line_number|
+    #   puts "#{line_number+1}. #{song.artist.name} - #{song.name} - #{song.genre.name}"
+    # end
   end
 
   def list_songs_by_artist
