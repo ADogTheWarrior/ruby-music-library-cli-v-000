32d8672a5d50c6a212acafe4722a404f5d539438
Adam Fudge <coolfudge@gmail.com>
2017-10-24 00:59:58 +0000

    automatically backed up by learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -16,7 +16,7 @@ class MusicLibraryController
     puts "To quit, type 'exit'."
     puts "What would you like to do?"
 
-    input = gets
+    input = ""
     while input != 'exit'
       input = gets
       case input
